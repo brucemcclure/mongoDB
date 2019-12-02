efd4fdba0a8478cc7edbc7aa7ff677d0e0235d23
Bruce McClure <brucemcc90@gmail.com>
2019-12-02 17:09:31 +1100

    remove an existing subdoc

--- users/test/subdocument_test.js
+++ users/test/subdocument_test.js
@@ -15,4 +15,66 @@ describe('subdocumets', () => {
       })
     })
   })
+
+  // Promise chaining.
+  // Create a user. Save. Update a user post. Save. Assert.
+
+  it('can add sub documents to an existing record', done => {
+    const hamo = new User({
+      name: 'Hamo',
+      posts: []
+    })
+    hamo
+      .save()
+      .then(() => User.findOne({ name: 'Hamo' }))
+      .then(user => {
+        user.posts.push({ title: 'New Post' })
+        return user.save()
+      })
+      .then(() => User.findOne({ name: 'Hamo' }))
+      .then(user => {
+        assert(user.posts[0].title === 'New Post')
+        done()
+      })
+  })
+
+  //   it('can remove an existing sub document', done => {
+  //     const hamo = new User({
+  //       name: 'Hamo',
+  //       posts: [{ title: 'New Post' }]
+  //     })
+  //     hamo
+  //       .save()
+  //       .then(() => User.findOne({ name: 'Hamo' }))
+  //       .then(user => {
+  //         const post = user.posts[0]
+  //         post.remove()
+  //         return user.save()
+  //       })
+  //       .then(() => User.findOne({ name: 'Hamo' }))
+  //       .then(user => {
+  //         assert(user.posts.length === 0)
+  //       })
+  //     done()
+  //   })
+  it('can remove an existing subdocument', done => {
+    const joe = new User({
+      name: 'Joe',
+      posts: [{ title: 'New Title' }]
+    })
+
+    joe
+      .save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then(user => {
+        const post = user.posts[0]
+        post.remove()
+        return user.save()
+      })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then(user => {
+        assert(user.posts.length === 0)
+        done()
+      })
+  })
 })
